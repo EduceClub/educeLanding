0f9bd64daa5d1f2ab77153987649179bca358707
Doug Kellermeyer <doug.kellermeyer@gmail.com>
2022-12-04 21:53:47 -0500

    updated build tab text, updated recall gif, and added share tab with gif

--- src/components/HowItWorks.js
+++ src/components/HowItWorks.js
@@ -4,6 +4,7 @@ import React, { useState } from 'react'
 import Buildon from '../assets/Buildon2.gif'
 import Recall from '../assets/Recall.gif'
 import Store2 from '../assets/Store2.gif'
+import Share from '../assets/Share.gif'
 import Box from '@mui/material/Box';
 import Stepper from '@mui/material/Stepper';
 import Step from '@mui/material/Step';
@@ -84,19 +85,6 @@ const withStyles = makeStyles((theme) => ({
 const HowItWorks = () => {
     const classes = withStyles();
 
-    const steps = [{
-        label: 'Store',
-        gif: Store2
-    },
-    {
-        label: 'Build',
-        gif: Buildon
-    },
-    {
-        label: 'Recall',
-        gif: Recall
-    }];
-
 
     const [activeStep, setActiveStep] = useState(0);
     const [value, setValue] = React.useState(0);
@@ -151,6 +139,7 @@ const HowItWorks = () => {
                             <Tab classes={{ root: classes.tabLabelSelected }} label="Store" {...a11yProps(0)} />
                             <Tab classes={{ root: classes.tabLabelSelected }} label="Build" {...a11yProps(1)} />
                             <Tab classes={{ root: classes.tabLabelSelected }} label="Recall" {...a11yProps(2)} />
+                            <Tab classes={{ root: classes.tabLabelSelected }} label="Share" {...a11yProps(2)} />
                         </Tabs>
                     </Box>
 
@@ -164,9 +153,7 @@ const HowItWorks = () => {
 
                     <div className={classes.tabsContentWrapper}>
                         <TabPanel className={classes.tabPanelElement} value={value} index={1}>
-                            <Typography className={classes.tabsDescription}>Share categories with others to help build up on your insights.
-                                Whether you have a work topic to share with colleagues and industry experts or you have a child's medical information you want to share with your partner.
-                                <span> <br /><br />Shared categories allow you to work with others to build updon your information.</span> </Typography>
+                            <Typography className={classes.tabsDescription}>Build upon your takeaways by adding columns and rows. This ability allows you to add additional thoughts or context to your information.</Typography>
 
                             <img className={classes.gif} src={Buildon} />
                         </TabPanel>
@@ -178,6 +165,13 @@ const HowItWorks = () => {
                             <Typography className={classes.tabsDescription}>Category tags allow you to see all the insights or information you store to a topic in one view.
                                 This helps you organize your information over time.</Typography>
                             <img className={classes.gif} src={Recall} />
+                        </TabPanel>
+                    </div>
+                    <div className={classes.tabsContentWrapper}>
+                        <TabPanel className={classes.tabPanelElement} value={value} index={3}>
+                            <Typography className={classes.tabsDescription}>Share cards with others to help build up on your insights. Whether you have a work topic to share with colleagues and industry experts or you have a child's medical information you want to share with your partner.<br/><br/>Shared cards and categories allow you to work with others to build upon your information.</Typography>
+
+                            <img className={classes.gif} src={Share} />
                         </TabPanel>
                     </div>
 
