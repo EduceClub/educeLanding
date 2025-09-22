bd03ba2e14abddfb857c539f730ccf41b0596f13
Alex Hetrick <ventuargames@gmail.com>
2025-09-22 14:52:26 -0400

    cleanup unnecessary files + better favicon

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
-import Layout from './components/Layout'
-import SectionOne from './components/SectionOne';
-import SectionTwo from './components/SectionTwo';
-import SectionThree from './components/SectionThree'
-import HowItWorks from './components/HowItWorks';
 import { createTheme, ThemeProvider } from "@mui/material";
 
+import Layout from "./components/Layout";
+import SectionOne from "./components/SectionOne";
+import SectionTwo from "./components/SectionTwo";
+// import SectionThree from './components/SectionThree'
+import HowItWorks from "./components/HowItWorks";
+
+import "./App.css";
+
 import ReactGA from "react-ga4";
-ReactGA.initialize('G-X5QZPD7RWC');
+ReactGA.initialize("G-X5QZPD7RWC");
 ReactGA.send("pageview");
 
 const educeTheme = createTheme({
-  palette: {
-    primary: {
-      main: "#AAF0C1"
-    },
-    secondary: {
-      main: "#000000"
-    }
-  },
+	palette: {
+		primary: {
+			main: "#AAF0C1",
+		},
+		secondary: {
+			main: "#000000",
+		},
+	},
 });
 
 function App() {
-  return (
-    <ThemeProvider theme={educeTheme}>
-      <div className="App">
-        <Layout>
-          <SectionOne />
-          <SectionTwo />
-          {/* <SectionThree /> */}
-          <HowItWorks />
-        </Layout>
-      </div>
-      </ThemeProvider>
-  );
+	return (
+		<ThemeProvider theme={educeTheme}>
+			<div className="App">
+				<Layout>
+					<SectionOne />
+					<SectionTwo />
+					{/* <SectionThree /> */}
+					<HowItWorks />
+				</Layout>
+			</div>
+		</ThemeProvider>
+	);
 }
 
 export default App;
